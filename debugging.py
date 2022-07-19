30618943781caa02c36f5889599b215888856d2c
CesarGBkR <yexotortuesprin@gmail.com>
2022-07-19 14:09:59 -0500

    Manejo de errores ejemplo practico en debugging.py

--- debugging.py
+++ debugging.py
@@ -1,14 +1,22 @@
 def divisors(num):
     divisors = []
-    for i in range(1, num + 1 ):
-        if num %i == 0:
-            divisors.append(i)
-    return divisors
+    try:
+        if num < 0 or num == 0:
+            raise ValueError("No se pueden ingresar números negativos ni cero")
+        for i in range(1, num + 1 ):
+            if num %i == 0:
+                divisors.append(i)
+        return divisors
+        
+    except ValueError as ve:
+        return ve
 
 def run():
-    num = int(input("Ingresa un número: "))
-    print(divisors(num))
-    print("Fin del programa")
-
+    try:
+        num = int(input("Ingresa un número: "))
+        print(divisors(num))
+        print("Fin del programa")
+    except ValueError as ve:
+        print("Debes ingresar un número")
 if __name__ =='__main__':
     run()
